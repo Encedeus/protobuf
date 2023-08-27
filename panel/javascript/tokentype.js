70f5c11890cb24dbb2a7d8d8c7237fbfc27e606b
OptimusePrime <karlo.vizec@skole.hr>
2023-08-27 13:37:14 +0200

    created definition files and generated source code in JS and Go for panel API request/response objects

---
+++ panel/javascript/tokentype.js
@@ -0,0 +1,20 @@
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+goog.provide('proto.panel.TokenType');
+
+/**
+ * @enum {number}
+ */
+proto.panel.TokenType = {
+  ACCESS_TOKEN: 0,
+  REFRESH_TOKEN: 1,
+  ACCOUNT_API_KEY: 2
+};
+
